bf14a400f1b482c0509a3f2bfb19406cf7b6e7c0
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-17 12:32:59 +0530

    Employee Attendance is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,10 @@
 public class EmployeeWage{
 public static void main(String [] args){
-System.out.println("EmployeeWage Details");
+int Is_present = 1;
+double empCheck = Math.floor(Math.random() * 10) % 2;
+if(Is_present == empCheck)
+System.out.println("Employee is Present");
+else
+System.out.println("Employee is absent");
 }
 }
