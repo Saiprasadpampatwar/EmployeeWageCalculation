87c79405eec0f4dd8fb316b16308e0f8c07de1ad
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-17 13:45:16 +0530

    Added Part time calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,16 @@
 public class EmployeeWage{
 public static void main(String [] args){
-int Is_present = 1;
+int Is_part_time =1;
+int Is_present = 2;
 int emp_rate_per_hour = 20;
 int empHrs = 0;
 int empWage = 0;
-double empCheck = Math.floor(Math.random() * 10) % 2;
-if(Is_present == empCheck)
+double empCheck = Math.floor(Math.random() * 10) % 3;
+if(empCheck == Is_part_time)
+{
+empHrs = 4;
+}
+else if (empCheck == Is_present)
 {
 empHrs = 8;
 }
