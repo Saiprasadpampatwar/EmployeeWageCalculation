cdb0f9c1da4bf356111e0c9da3b3d0df87c29b65
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-19 15:44:20 +0530

    Refractured using class method and class variable

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,34 +1,57 @@
-public class EmployeeWage{
-public static final int Is_part_time = 1;
-public static final int Is_present = 2;
-public static final int emp_rate_per_hour = 20;
-public static final int Num_of_working_days = 20;
-public static final int Max_hrs_in_month = 100;
-public static void main(String [] args){
-int empHrs = 0;
-int empWage = 0;
-int totalEmpWage = 0;
-int totalWorkingDays = 0;
-int totalEmpHrs = 0;
-while(totalEmpHrs<=Max_hrs_in_month && totalWorkingDays < Num_of_working_days)
-{
-totalWorkingDays++;
-int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-switch (empCheck)
-{
-case Is_part_time:
-empHrs = 4;
-break;
-case Is_present:
-empHrs = 8;
-break;
-default:
-empHrs = 0;
+public class EmployeeWage {
+
+	public static final int No_of_working_days = 20;
+	public static final int Employee_present = 1;
+	public static final int Employee_absent = 0;
+	public static final int Employee_part_time = 2;
+	public static final int wage_per_hour = 20;
+	public static final int hours_in_a_day = 8;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double totalWage = CalculateEmpWage();
+		System.out.println("Monthly Income of Employee : "+totalWage);
+
+	}
+	public static double CalculateEmpWage() {
+		double total_monthly_wage;
+		int total_emp_hrs = 0;
+		double total_working_days = 0;
+		System.out.println("Hello Employee");
+		while(total_emp_hrs < 100 && total_working_days<No_of_working_days)
+		{
+		int emp_hrs = 0;
+		double days = 0;
+		int employee_presence = (int) (Math.floor(Math.random() * 3));
+		if (employee_presence == Employee_present) {
+			emp_hrs = 8;
+			days = 1;
+		}
+		else if (employee_presence == Employee_part_time)
+		{
+			emp_hrs = 4;
+			days = 0.5 ;
+		}
+		else {
+			emp_hrs = 0;
+			days = 0;
+		}
+		total_emp_hrs+=emp_hrs;
+		total_working_days+=days;
+		if(total_emp_hrs>100 ) {
+			total_emp_hrs=100;
+		}
+		if (total_working_days>20) {
+			total_working_days=20;
+		}
+		}
+		System.out.println("No. of Days: "+total_working_days);
+		System.out.println("No. of working hrs: "+total_emp_hrs);
+		total_monthly_wage = total_emp_hrs * wage_per_hour;
+		return total_monthly_wage;
+
+	}
+
+
 }
-totalEmpHrs+=empHrs;
-System.out.println("Day#: "+ totalWorkingDays + "Emp Hr: " + empHrs);
-}
-totalEmpWage = totalEmpHrs * emp_rate_per_hour;
-System.out.println("Total Emp Wage: "+totalEmpWage);
-}
-}
+
