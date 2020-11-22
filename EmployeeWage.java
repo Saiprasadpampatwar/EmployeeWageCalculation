4d63592a4f9e73cca43d26e11de430a2d0fd492d
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-22 08:52:40 +0530

    Used Instance variable instead of function parameter

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,35 +1,21 @@
 import java.util.Scanner;
 
 public class EmployeeWage {
-	public static Scanner sc = new Scanner(System.in);
-
-	//public static final int No_of_working_days = 20;
 	public static final int Employee_present = 1;
 	public static final int Employee_absent = 0;
 	public static final int Employee_part_time = 2;
-	//public static final int Wage_per_hour = 20;
-	//public static final int Hours_in_a_day = 8;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Enter the name of company");
-		String companyName = sc.next();
-		System.out.println("Enter the no of working days");
-		int no_of_working_days = sc.nextInt();
-		System.out.println("Enter the wage per hour");
-		int wage_per_hour = sc.nextInt();
-		System.out.println("Enter hours in a day");
-		int hours_in_a_day = sc.nextInt();
-		double totalWage = CalculateEmpWage(no_of_working_days,wage_per_hour,hours_in_a_day);
-		System.out.println("Monthly Income of Employee : "+totalWage);
-
-	}
-	public static double CalculateEmpWage(int no_of_working_days, int wage_per_hour, int hours_in_a_day ) {
+	String nameOfCompany;
+	int no_of_working_days;
+	int wage_per_hour;
+	int hours_in_a_day;
+	public static Scanner sc = new Scanner(System.in);
+	
+	public double CalculateEmpWage() {
 		double total_monthly_wage;
 		int total_emp_hrs = 0;
 		double total_working_days = 0;
 		System.out.println("Hello Employee");
-		while(total_emp_hrs < 100 && total_working_days<no_of_working_days)
+		while(total_working_days<no_of_working_days)
 		{
 		int emp_hrs = 0;
 		double days = 0;
@@ -63,5 +49,21 @@ public class EmployeeWage {
 
 	}
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeWage e1 = new EmployeeWage();
+		System.out.println("Enter the name of company");
+		e1.nameOfCompany = sc.next();
+		System.out.println("Enter the no of working days");
+		e1.no_of_working_days = sc.nextInt();
+		System.out.println("Enter the wage per hour");
+		e1.wage_per_hour = sc.nextInt();
+		System.out.println("Enter hours in a day");
+		e1.hours_in_a_day = sc.nextInt();
+		double totalWage = e1.CalculateEmpWage();
+		System.out.println("Monthly Income of Employee : "+totalWage);
 
+	}
 }
+	
+
