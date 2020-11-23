f4e75ece7118392539a2623971e5957821c49266
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-23 15:11:40 +0530

    [Saiprasad]Refactored: Used ArrayList instead of Array

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeWage {
@@ -5,25 +6,34 @@ public class EmployeeWage {
 	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter how many comanies do you want to enter:");
-		int noOfCompany = sc.nextInt();
-		CompanyEmpWage [] DifferentCompanies = new CompanyEmpWage[noOfCompany];
-		for (int i = 0; i < DifferentCompanies.length; i++) {
-			System.out.println("Enter Name of Comany:");
-			String name = sc.next();
-			System.out.println("Enter no of working days:");
-			int workingDays = sc.nextInt();
-			System.out.println("Enter wage per hour");
-			int wagePerHour = sc.nextInt();
-			System.out.println("Enter Working Hrs in a day:");
-			int workingHrsInDay = sc.nextInt();
-			CompanyEmpWage e = new CompanyEmpWage(name, workingDays, wagePerHour, workingHrsInDay);
-			double montlyWage = e.CalculateEmpWage();
-			System.out.println(name + "'s Monthly Wage is : "+ montlyWage);
-			DifferentCompanies[i] = e;
-		}
-	
-		
+		ArrayList<CompanyEmpWage> DifferentCompanies = new ArrayList<>();
+		int select = 1;
+		while(select == 1)
+		{
+			System.out.println("Choose\n 1.Add a company\n 2.Exit");
+			int option = sc.nextInt();
+			switch(option) {
+			case 1:
+				System.out.println("Enter Name of Comany:");
+				String name = sc.next();
+				System.out.println("Enter no of working days:");
+				int workingDays = sc.nextInt();
+				System.out.println("Enter wage per hour");
+				int wagePerHour = sc.nextInt();
+				System.out.println("Enter Working Hrs in a day:");
+				int workingHrsInDay = sc.nextInt();
+				CompanyEmpWage e = new CompanyEmpWage(name, workingDays, wagePerHour, workingHrsInDay);
+				double montlyWage = e.CalculateEmpWage();
+				System.out.println(name + "'s Monthly Wage is : "+ montlyWage);
+				DifferentCompanies.add(e);
+				select = 1;
+				break;
+			case 2:
+				System.out.println("Thank You");
+				select = 0;
+				break;
+			}
+		}		
 
 	}
 }
